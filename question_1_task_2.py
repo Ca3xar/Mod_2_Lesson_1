386a6e5f4a56b6ec7ca3e7854227a80aa39832fd
Ca3xar <Oster.corey@gmail.com>
2024-10-08 13:37:55 -0500

    Add files via upload

---
+++ question_1_task_2.py
@@ -0,0 +1,6 @@
+mood = input ("How do you feel today?")
+
+if mood == ("happy"):
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
---
+++ question_3_task_1.py
@@ -0,0 +1,5 @@
+bread = 5
+peanut_butter = 4
+jelly = 4
+
+print(jelly + peanut_butter + bread)
